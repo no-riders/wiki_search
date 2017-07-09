4c239689938e3063f55ac3a829ae9d6ec38e7f93
Yev <evlogosha@gmail.com>
2017-07-09 12:37:10 +0300

    js tweaks

--- js/script.js
+++ js/script.js
@@ -42,7 +42,8 @@ btn.addEventListener('click', function() {
 query.addEventListener('keypress', function(event) {
     if (event.keyCode == 13) {
         event.preventDefault();
-        search();
+        btn.click();
+        // search();
     }
 })
 
