a05c7c3bab6bd854312824c57de4a3a6591ad118
Yev <evlogosha@gmail.com>
2017-09-23 16:27:37 +0300

    es6

--- js/script.js
+++ js/script.js
@@ -1,52 +1,51 @@
-var query = document.getElementById('search_field');
-var btn = document.getElementById('btn');
-var header = document.getElementsByTagName('HEADER')[0];
-var main = document.getElementsByTagName('MAIN')[0];
-var footer = document.getElementsByTagName('FOOTER')[0];
+const query = document.getElementById('search_field');
+const btn = document.getElementById('btn');
+const header = document.getElementsByTagName('HEADER')[0];
+const main = document.getElementsByTagName('MAIN')[0];
+const footer = document.getElementsByTagName('FOOTER')[0];
 
-query.addEventListener('keydown', function() {
+query.addEventListener('keydown', () => {
     btn.style.display = "inline-block";
 })
 
-query.addEventListener('keypress', function(event) {
+query.addEventListener('keypress', event => {
     if (event.keyCode == 13) {
         event.preventDefault();
         btn.click();
     }
 })
 
-btn.addEventListener('click', function() {
-    var request = new XMLHttpRequest();
-    var findWiki = query.value;
-    request.open('GET', 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + findWiki + '');
-    request.onload = function() {
+btn.addEventListener('click', () => {
+    const request = new XMLHttpRequest();
+    const findWiki = query.value;
+    request.open('GET', `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${findWiki}`);
+    request.onload = () => {
       footer.style.display = 'block';
-        var title = [];
-        var content = [];
-        var wikiUrl = [];
-        var listTitles = document.getElementById('list').getElementsByTagName('h4');
-        var listContent = document.getElementById('list').getElementsByTagName('p');
-        var url = document.getElementById('list').getElementsByTagName('a');
-        var data = JSON.parse(request.responseText);
-        var arr = Object.keys(data.query.pages).map(function(e) { //obj to arr
-           return [Number(e), data.query.pages[e]];
-        })
+        const title = [];
+        const content = [];
+        const wikiUrl = [];
+        const listTitles = document.getElementById('list').getElementsByTagName('h4');
+        const listContent = document.getElementById('list').getElementsByTagName('p');
+        const url = document.getElementById('list').getElementsByTagName('a');
+        const data = JSON.parse(request.responseText);
+        const arr = Object.keys(data.query.pages).map(e => //obj to arr
+        [Number(e), data.query.pages[e]]);
 
         header.className = 'after_search';
         main.style.display = 'block';
-        for (var i = 0; i < arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             title.push(arr[i][1].title);
             content.push(arr[i][1].extract);
             wikiUrl.push(arr[i][1].pageid);
             listTitles[i].innerHTML = title[i];
             listContent[i].innerHTML = content[i];
-            url[i].href = 'https://en.wikipedia.org/?curid=' + wikiUrl[i];
+            url[i].href = `https://en.wikipedia.org/?curid=${wikiUrl[i]}`;
         }
     }
     request.send()
 });
 
-document.getElementsByClassName('clear')[0].addEventListener('click', function() {
+document.getElementsByClassName('clear')[0].addEventListener('click', () => {
   footer.style.display = 'none';
     btn.style.display = 'none';
     header.classList.remove('after_search');
